ad21f5fb8c41039bd814008539b5d49cee63fb3b
bharathk-reddy <kallurubharathreddy.14@gmail.com>
2025-12-09 12:28:55 -0600

    task & band_generator

---
+++ day01/band_generator.py
@@ -0,0 +1,7 @@
+#Band generator
+print("Hey buddy! Welcome to BanD naME geneRatOR")
+city_name = input("What is your born city name? \n")
+print("Birth city is "+city_name )
+pet_name = input("What is your pet name? \n")
+print("pet name is "+pet_name)
+band_name = print("Your band name is gonna be: "+city_name+" "+pet_name)
---
+++ day01/task.py
@@ -0,0 +1,35 @@
+#task1
+#default first python line of code
+print("Hello world!") 
+
+#task2
+#string concat
+print("Hi Bharath..!!!" + " " +"Hello world!") 
+
+#task3
+#input function & string concat
+print("Hello! " + input("What is your name?"))
+
+#task4 
+#variable & input function
+name = input("What is your name?")
+print(len(name))
+james = 123456798
+print(james)
+name = "Bharath"
+print(name)
+name = "Kalluru"
+print(name)
+username = input("Enter your username?")
+length = len(username)
+print(length)
+
+#task5
+#rules of variable naming in Python
+name = "Bharath" #descriptive names
+# length 1 = len(name) - no spaces
+length1 = len(name) #can use numbers in variable name
+#1length = len(name) #don't start the variable name with a number
+# len = variable #don't use spcl words as a variable
+#always use simple words
+print(length1)
